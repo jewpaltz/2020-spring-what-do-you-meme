122e18f078631bd2b06c305ba2bf5c4b58a947fa
Moshe Plotkin <plotkinm@newpaltz.edu>
2020-02-17 15:19:10 -0500

    added some express code and npm stuff

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app
+    .get('/', (req, res) => res.send('This class is awesome!') );
+
+
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
