124f2d11f2769a76eec169b288016a9044e443db
Moshe Plotkin <plotkinm@newpaltz.edu>
2020-03-02 15:17:32 -0500

    font awesome

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bulma';
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
 
