666da879c5d5af4c21321385adcfc87bad00aed8
Moshe Plotkin <plotkinm@newpaltz.edu>
2020-03-09 15:13:33 -0400

    managing quote cards

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -7,7 +7,11 @@ const quoteCards = require('../models/quoteCards');
 const router = express.Router();
 
 router
-    .get('/quoteCards', (req, res) => res.send(quoteCards) );
+    .get('/quoteCards', (req, res) => res.send(quoteCards) )
+    .post('/quoteCards', (req, res) => {
+        quoteCards.add(req.query.text);
+        res.send(quoteCards.list[quoteCards.list.length - 1]);
+    })
 
 
 module.exports = router;
--- server/models/quoteCards.js
+++ server/models/quoteCards.js
@@ -1,6 +1,20 @@
-const quoteCards = [
-    "This class is awesome",
-    "Come to Shabbat 200"
+const quoteCards =[
+    'For me, math class is like watching a foreign movie without subtitles.',
+    'Maybe if we tell people the brain is an app, they will start using it.',
+    'When nothing goes right, go left.',
+    'A cop pulled me over and told me “Papers”, so I said “Scissors, I win!” and drove off.',
+    'Mom: Why is everything on the floor? Me: Gravity!',
+    'Sure, I do marathons. On Netflix.',
+    'Did it for the memories – totally worth it!',
 ];
 
-module.exports = quoteCards;
+function add(text) {
+    quoteCards.push(text);
+}
+
+module.exports = {
+    list: quoteCards,
+    add: add
+}
+
+;
