78acbcfbdf2eda57474349e7e65c14dcf3c28205
Moshe Plotkin <plotkinm@newpaltz.edu>
2020-03-12 15:20:27 -0400

    serving up the client code from the server Also created a catch all to serve index.html to handle deep linking with the history router

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const gameController = require('./controllers/game');
+const path = require('path')
 
 
 const app = express();
@@ -8,8 +9,15 @@ const port = 3000;
 app
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
+    .use(express.static( __dirname + '/../client/dist'))
     .get('/', (req, res) => res.send('This class is awesome!') )
     .use('/game', gameController)
+
+    .use((req, res) => {
+        const homePath = path.join( __dirname , '/../client/dist/index.html');
+        console.log(homePath);
+        res.sendFile(homePath)
+    })
 
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
