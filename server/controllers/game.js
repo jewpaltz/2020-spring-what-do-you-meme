012aa6185d9238c22263468f3f3230fcde2d767c
Moshe Plotkin <plotkinm@newpaltz.edu>
2020-03-12 15:01:16 -0400

    using json parser midleware

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -7,9 +7,13 @@ const quoteCards = require('../models/quoteCards');
 const router = express.Router();
 
 router
+    .use('/quoteCards', (req, res, next) => {
+        console.log({ method: req.method, body: req.body });
+        next();
+    })
     .get('/quoteCards', (req, res) => res.send(quoteCards) )
     .post('/quoteCards', (req, res) => {
-        quoteCards.add(req.query.text);
+        quoteCards.add(req.body.text);
         res.send(quoteCards.list[quoteCards.list.length - 1]);
     })
 
--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const app = express();
 const port = 3000;
 
 app
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
     .get('/', (req, res) => res.send('This class is awesome!') )
     .use('/game', gameController)
 
