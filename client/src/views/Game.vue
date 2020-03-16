72e63911c49971a40b3293394bbd14db374b476a
Conor O'Riordan <oriordac1@hawkmail.newpaltz.edu>
2020-03-16 11:34:00 -0400

    Fix bug with v-binding and Init( )

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -11,20 +11,20 @@
       <div class="card">
       <div class="card-content">
         <ul>
-          <li v-for="player in Players" :key="player.Name">
+          <li v-for="player in Game.Players" :key="player.Name">
             {{player.Name}} {{player.Score}}
           </li>
         </ul>
       </div>
 
-      <img :src="CurrentPicture"  class="card-image"/>
+      <img :src="Game.CurrentPicture"  class="card-image"/>
     </div>
 
   </div>
 </template>
 
 <script>
-import { Players, PictureDeck, CurrentPicture, Init } from "../models/Game";
+import * as Game from "../models/Game";
 
 
 export default {
@@ -33,13 +33,8 @@ export default {
     Init();
   },
   data:()=>({
-    Players,
-    PictureDeck,
-    CurrentPicture
-  }),
-  components: {
-    
-  }
+    Game
+  })
 }
 </script>
 
