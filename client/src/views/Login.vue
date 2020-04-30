c516a3d77a6a3c83374d4ed077cb112a7e1dd3c0
plotkinm <plotkinm@Admin.newpaltz.edu>
2020-04-30 12:31:16 -0400

    google login

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -26,12 +26,17 @@
             Login
             </button>
         </p>
+            <button class="button is-primary" @click.prevent="google_login" >
+                Login with Google
+            </button>
         </div>
   </form>
 </template>
 
 <script>
 import { Login } from "../models/Users";
+const GOOGLE_CLIENT_ID = "54172377215-0k6du17ds6up5gm3i1v0d62ehcmno6v2.apps.googleusercontent.com";
+let auth2 = null;
 
 export default {
     data(){
@@ -39,6 +44,23 @@ export default {
             email: '',
             password: '',
             error: ''
+        }
+    },
+    created(){
+        const googleScriptTag = document.createElement('script')
+        googleScriptTag.setAttribute('src', 'https://apis.google.com/js/api:client.js')
+        document.head.appendChild(googleScriptTag)
+
+        googleScriptTag.onload = () => {
+            // the global gapi variable is created by loading that script
+            gapi.load('auth2', () => {
+                auth2 = gapi.auth2.init({
+                    client_id: GOOGLE_CLIENT_ID,
+                    cookiepolicy: 'single_host_origin',
+                    scope: 'profile email'
+                })
+            })
+
         }
     },
     methods: {
@@ -49,6 +71,22 @@ export default {
             } catch (error) {
                 this.error = error;
             }
+        },
+        google_login(){
+                auth2.signIn()
+                .then(googleUser =>{
+                    console.log(googleUser);
+                    
+                    const profile = googleUser.getBasicProfile();
+                    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+                    console.log('Full Name: ' + profile.getName());
+                    console.log('Given Name: ' + profile.getGivenName());
+                    console.log('Family Name: ' + profile.getFamilyName());
+                    console.log("Image URL: " + profile.getImageUrl());
+                    console.log("Email: " + profile.getEmail());
+                } )
+                .catch(error => console.error(error))
+
         }
     }
 }
