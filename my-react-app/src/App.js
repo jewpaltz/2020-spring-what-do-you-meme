63e66a2556ae5fc84e479aebad7c9d1df1fa8e6c
plotkinm <plotkinm@Admin.newpaltz.edu>
2020-05-07 14:33:17 -0400

    Routing is working

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 import MyNav from "./components/Nav";
 
+const About = ()=> <h1 className="title is-1">About Page</h1>
+const Game = ()=> <h1 className="title is-1">Game Page</h1>
+const Home = ()=> <h1 className="title is-1">Home Page</h1>
+const NotFound = ()=> <h1 className="title is-1">404 - Sorry. Not Sorry. Watch where you are going!</h1>
+
 function App() {
   return (
-    <div className="App">
-      <MyNav />
-      
-    </div>
+    <Router>
+      <div className="App">
+        <MyNav />
+        
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
