bfb11fe2ec257ef230682dc297d4bcf9a3e5ce66
plotkinm <plotkinm@Admin.newpaltz.edu>
2020-05-07 11:37:41 -0400

    Routing

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
 import MyNav from "./components/Nav";
 
+const About = ()=> <h1 className="title is-1">About Page</h1>
+const Game = ()=> <h1 className="title is-1">Game Page</h1>
+const Home = ()=> <h1 className="title is-1">Home Page</h1>
+
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <MyNav />
-      
-    </div>
+
+      <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>      
+    </Router>
   );
 }
 
